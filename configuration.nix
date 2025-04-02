6fe5918bbbab98c1febc1deade5025a2f5b84640
Fernando Perry <fernandoperry1234@gmail.com>
2025-04-02 14:56:49 +0700

    added flake.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -10,16 +9,26 @@
             ./hardware-configuration.nix
         ];
 
-    # Bootloader.
+    # == Bootloader == #
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
+    # == Networking == #
     networking.hostName = "nixos";
-
-    # Configure network proxy if necessary
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = true;
+    networking.networkmanager = {
+        enable = true;
+        insertNameservers = ["1.1.1.1" "8.8.8.8" "8.8.4.4"];
+    };
+    networking.networkmanager.dns = "none";
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+    # == Environment == #
     environment.variables = {
         EDITOR = "nvim";
         MANPAGER = "nvim +Man!";
@@ -28,30 +37,11 @@
         NIXOS_OZONE_WL = "1";
     };
 
-    # Set your time zone.
+    # == Basic system settings == #
     time.timeZone = "Asia/Jakarta";
-
-    # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
-
-    services.xserver.enable = false;
-
-    # Enable the KDE Plasma Desktop Environment.
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
-
-    # Configure keymap in X11
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-    };
-
-    # Enable CUPS to print documents.
-    services.printing.enable = false;
-
     hardware.bluetooth.enable = true;
 
-    # Enable sound with pipewire.
     security.rtkit.enable = true;
     services.pipewire = {
         enable = true;
@@ -61,17 +51,25 @@
         jack.enable = true;
     };
 
-    # enable zram
     zramSwap = {
         enable = true;
         algorithm = "zstd";
         memoryPercent = 100;
     };
 
-    # Enable touchpad support (enabled default in most desktopManager).
+    # == Servies (Systemd stuff is here too) == #
+    services.xserver.enable = false;
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+    services.printing.enable = false;
     services.libinput.enable = true;
+    # services.openssh.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
+    # == User use ‘passwd’ to set password == #
     users.users.goad = {
         isNormalUser = true;
         description = "goad";
@@ -81,14 +79,9 @@
         ];
     };
 
-    # enable zsh
-    programs.zsh.enable = true;
-
-    # Allow unfree packages
+    # == For Program and stuff == #
     nixpkgs.config.allowUnfree = true;
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
+    programs.zsh.enable = true;
     environment.systemPackages = with pkgs; [
         pkgs.tmux
         pkgs.neovim
@@ -127,32 +120,10 @@
     #   enableSSHSupport = true;
     # };
 
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    networking.firewall.enable = true;
-    networking.networkmanager = {
-        enable = true;
-        insertNameservers = ["1.1.1.1" "8.8.8.8" "8.8.4.4"];
-    };
-    networking.networkmanager.dns = "none";
-
-    # autodelete some old version
+    # == MISC ==
     nix.gc.automatic = true;
     nix.gc.dates = "daily";
     nix.gc.options = "--delete-older-than 7d";
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "25.05"; # Did you read the comment?
+    nix.settings.experimental-features = ["nix-command" "flakes"];
+    system.stateVersion = "25.05";
 }
