449e79072a00ca2f7e90f2cc8bad5a4c9d5afa41
Fernando Perry <fernandoperry1234@gmail.com>
2025-04-15 20:01:51 +0700

    use latest kernel

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -11,6 +11,7 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.kernel.sysctl."kernel.sysrq" = 1;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
     # == Networking == #
     networking.hostName = "nixos";
